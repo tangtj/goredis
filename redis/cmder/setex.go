01ef89ddc4e58fd70614989d04e531c2a91af527
tangtj <79408208+tangtj@users.noreply.github.com>
2021-11-19 00:59:28 +0800

    加入 setex 命令 (#7)
    * feat : 加入 setex 命令

---
+++ redis/cmder/setex.go
@@ -0,0 +1,33 @@
+package cmder
+
+import (
+	"goredis/inf"
+	"goredis/redis/reply"
+	"strconv"
+	"time"
+)
+
+func SetEX(c *inf.Client, _ string, args [][]byte) inf.Reply {
+	d := c.Db.GetData()
+	expire := c.Db.GetExpire()
+
+	// 判断是否有效
+	if len(args) != 3 {
+		return reply.MakeErrReply("ERR wrong number of arguments for 'setex' command")
+	}
+
+	key := string(args[0])
+
+	s := string(args[1])
+	second, err := strconv.Atoi(s)
+	if err != nil {
+		return reply.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	now := time.Now().Add(time.Duration(second) * time.Second).UnixMilli()
+
+	expire.Add(key, now)
+
+	value := string(args[2])
+	d.Add(key, value)
+	return reply.OKReply
+}
