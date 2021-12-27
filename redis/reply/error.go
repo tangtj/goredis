56341b6427b16bee51992d056bfc5f4d0f64e466
tangtj <79408208+tangtj@users.noreply.github.com>
2021-12-27 22:20:44 +0800

    feat : add error (#16)
    feat : add error

--- redis/reply/error.go
+++ redis/reply/error.go
@@ -2,6 +2,7 @@ package reply
 
 import (
 	"fmt"
+	"goredis/inf"
 	"sync"
 )
 
@@ -9,14 +10,28 @@ var ErrNum = MakeErrReply("ERR value is not an integer or out of range")
 
 var _argsErrStr = "ERR wrong number of arguments for '%s' command"
 
-var _errMap sync.Map
+var _unknownErrStr = "ERR unknown command `%s`, with args beginning with:"
+
+var _errArgsMap sync.Map
+
+var _errUnknownMap sync.Map
 
 func ErrArgsNumber(command string) *ErrReply {
-	if v, ok := _errMap.Load(command); ok {
+	if v, ok := _errArgsMap.Load(command); ok {
 		return v.(*ErrReply)
 	} else {
 		e := MakeErrReply(fmt.Sprintf(_argsErrStr, command))
-		_errMap.Store(command, e)
+		_errArgsMap.Store(command, e)
+		return e
+	}
+}
+
+func ErrUnknownMap(command string) inf.Reply {
+	if v, ok := _errUnknownMap.Load(command); ok {
+		return v.(*ErrReply)
+	} else {
+		e := MakeErrReply(fmt.Sprintf(_unknownErrStr, command))
+		_errUnknownMap.Store(command, e)
 		return e
 	}
 }
